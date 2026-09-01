aa14bd8ffd77989bb9c407112d97bb1815da21e5
yezranaseer <yezranseer098@gmail.com>
2026-09-02 00:43:39 +0530

    Update trip feasibility interfaces

---
+++ src/trip-feasibility/interfaces/travel-style.interface.ts
@@ -0,0 +1,10 @@
+import {
+  TRANSPORTATION_STYLES,
+  TRAVEL_STYLES,
+} from '../constants/trip-feasibility.constants';
+
+// Tourist spending style used to estimate food and accommodation.
+export type TravelStyle = (typeof TRAVEL_STYLES)[number];
+
+// Tourist transport preference. Transport cost itself is already supplied in route segments.
+export type TransportationStyle = (typeof TRANSPORTATION_STYLES)[number];
