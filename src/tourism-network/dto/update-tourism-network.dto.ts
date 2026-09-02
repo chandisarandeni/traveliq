ac4635c3c6bf815212f57727a0a5a7d2be9072a5
F S Munthasir <suhamunthasir123@gmail.com>
2026-09-02 09:24:11 +0530

    Fix test issue in generated DTO files

--- src/tourism-network/dto/update-tourism-network.dto.ts
+++ src/tourism-network/dto/update-tourism-network.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTourismNetworkDto } from './create-tourism-network.dto';
-
-export class UpdateTourismNetworkDto extends PartialType(CreateTourismNetworkDto) {}
+export class UpdateTourismNetworkDto {}
--- src/travel-plan-ranking/dto/update-travel-plan-ranking.dto.ts
+++ src/travel-plan-ranking/dto/update-travel-plan-ranking.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTravelPlanRankingDto } from './create-travel-plan-ranking.dto';
-
-export class UpdateTravelPlanRankingDto extends PartialType(CreateTravelPlanRankingDto) {}
+export class UpdateTravelPlanRankingDto {}
